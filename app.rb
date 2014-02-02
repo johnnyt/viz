79600b7fb01d30cb7fe3a9819e07e11d57f67e26
JohnnyT <ubergeek3141@gmail.com>
2014-02-02 08:31:43 -0700

    Add routes to write Amber packages to disk

--- app.rb
+++ app.rb
@@ -8,6 +8,15 @@ class VizApp < ::Sinatra::Base
   end
 
   get '/' do
-    json :message => 'hello world'
+    File.read(File.expand_path '../public/index.html', __FILE__)
+    #json :message => "Hello world! (from #{RUBY_ENGINE})"
+  end
+
+  # Write Amber packages to disk
+  put '/packages/:type/:package' do
+    File.open("./packages/#{params[:type]}/#{params[:package]}", 'w+') do |file|
+      file.write request.body.read
+    end
+    'success'
   end
 end
